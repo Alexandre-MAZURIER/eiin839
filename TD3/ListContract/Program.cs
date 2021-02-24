cf53dfa383f73cb8f925d6c2f757d1a25d5b14be
Alexandre MAZURIER <a.mazurier83@gmail.com>
2021-02-24 15:19:12 +0100

    TD3.1

---
+++ TD3/ListContract/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListContract
+{
+    class Program
+    {
+
+        static readonly HttpClient client = new HttpClient();
+
+        static async Task Main()
+        {
+            // Call asynchronous network methods in a try/catch block to handle exceptions.
+            try
+            {
+                string url = "https://api.jcdecaux.com/vls/v3/";
+                string data = "contracts";
+                string apiKey = "76e5a6818bf2b41854f1f851546c5536616a40c3";
+                HttpResponseMessage response = await client.GetAsync(url + data + "?apiKey=" + apiKey);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                // Above three lines can be replaced with new helper method below
+                // string responseBody = await client.GetStringAsync(uri);
+
+                Console.WriteLine(responseBody);
+                Console.ReadLine();
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+        }
+    }
+}
